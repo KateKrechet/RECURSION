ce8138fbf628d026ce6da087aef5ba1c44a60f5a
KateKrechet <k.krechet@yandex.ru>
2021-08-25 21:12:53 +0300

    CLASS WORK Modification

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,8 +1,8 @@
 ﻿#include<iostream>
 using namespace std;
 //#define FLOOR
-#define FACTORIAL
-#define POWER
+//#define FACTORIAL
+//#define POWER
 #define FIBONACHCHI
 #define tab "\t"
 
@@ -20,25 +20,36 @@ void elevator(int floor)
 
 long long int factorial(int n)
 {
-	if (n < 1) return 0;
-	else if (n == 1) return 1;
-	else return n * factorial(n - 1);
+	return n == 0 ? 1 : n * factorial(n - 1);
 }
 double power(int a, int n)
 {
-	if (a == 0 && n == 0) return 0;
-	else if (n == 0) return 1;
-	else if (n < 0) return 1 / power(a, n * (-1));
-	else return a * power(a, n - 1);
+	/*if (n == 0) return 1;
+	if (n < 0) return 1. / power(a, n + 1);
+	return a * power(a, n - 1);*/
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1. / a * power(a, n + 1);
+
 }
-long long int fibonachchi(int n)
+//void fibonachchi(int n, int a = 0, int b = 1);
+void fibonachchi(int n/*, int a, int b*/)
 {
-	if (n == 0) return 0;
-	else if (n == 1 || n == 2) return 1;
-	else return fibonachchi(n - 2) + fibonachchi(n - 1);
+	static int a = 0, b = 1, c = 1;
+	if (a > n) return;
+	cout << a << "\t";
+	/*a = b;
+	b = c;
+	c = a + b;*/
+	c = (a = b) + (b = c);
+	fibonachchi(n);
 }
 
-
+void fibonachchi2(int n2/*, int a, int b*/)
+{
+	static int a = 0, b = 1, c = 1;
+	cout << a << "\t";
+	c = (a = b) + (b = c);
+	fibonachchi(n2);
+}
 
 void main()
 {
@@ -62,28 +73,19 @@ main();*/
 	int number1;
 	int n;
 	cout << "Введите число "; cin >> number1;
-	cout << "Введите степень "; cin >> n;
+	cout << "Введите показатель степени "; cin >> n;
 	cout << "Результат:" << power(number1, n) << endl;
 #endif // POWER
 #ifdef FIBONACHCHI
-	int n1;
-	cout << "Введите количество элементов последовательности Фибоначчи "; cin >> n1;
-	cout << "Вывод ряда Фибоначчи для заданного количества элементов" << endl;
-	for (int i = 1; i <= n1; i++)
-	{
-		cout << fibonachchi(i) << tab;
-	}
+	int n;
+	cout << "Введите значение, до которого надо выводить ряд Фибоначчи "; cin >> n;
+	fibonachchi(n);
 	cout << endl;
-	unsigned long long n2 = power(2, 64);
-	unsigned long long int limit;
-	cout << "Введите значение, до которого надо выводить ряд Фибоначчи "; cin >> limit;
-	for (int i = 1; i <= n2; i++)
+	int n2;
+	cout << "Введите количество элементов ряда Фибоначчи "; cin >> n2;
+	for (int i = 0; i < n2; i++)
 	{
-		if (fibonachchi(i) <= limit)
-		{
-			cout << fibonachchi(i) << tab;
-		}
-		else break;
+		fibonachchi2(i);
 	}
 #endif // FIBONACHCHI
 
